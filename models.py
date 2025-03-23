fcc2ab34a310198f51553227ce2d1698db60d280
nvson.red <nvson.red@gmail.com>
2025-03-23 23:43:09 +0700

    init

---
+++ models.py
@@ -0,0 +1,18 @@
+from enum import unique
+from symtable import Class
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+db = SQLAlchemy()
+
+class User(UserMixin, db.Model):
+    id = db.Colum(db.Integer, primary_key=True)
+    username = db.Colum(db.String(150), unique=True, nullable=False)
+    password = db.Colum(db.String(150), unique=True)
+
+class Task(db.Model):
+    id = db.Colum(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Boolean, default=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
